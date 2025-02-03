88fa74993391e7997f1419d22d06a993396631d3
arp1tk <arpitkukrety27@gmail.com>
2025-02-03 22:03:57 +0530

    homepage tests

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,4 +1,12 @@
 module.exports = {
-    testEnvironment: "jsdom",
-  };
-  
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Map @/ to src/
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(gsap|gsap/.*))'
+  ],
+  testEnvironment: 'jsdom',
+};
